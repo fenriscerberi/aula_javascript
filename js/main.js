2452357e1fe237313ed59122eda1aad1932e6620
unknown <kaike.spencer@gmail.com>
2020-08-26 13:06:40 -0300

    A1 Condicionais, laços de repetição e Date

--- js/main.js
+++ js/main.js
@@ -1,8 +1,40 @@
+ 
+ var d = new Date();
+ alert(d.getDay());
+ alert(d.getMonth());
+ alert(d.getHours());
+ alert(d.getMinutes());
+ 
+ /*
+ var count;
+ for (count = 0; count <=5; count++){
+	 alert (count);
+ };
+ */
+ 
+ /*
+ var count = 0;
+ while (count <= 5){
+	 console.log(count);
+	 alert (count);
+	 count++;
+ };
+ */
+ 
+ /*
+var idade = prompt("qual sua idade");
 
-
+if (idade >= 18){
+	alert ("maior de idade");
+}else{
+	alert ("menor de idade");
+}
+*/
+/*
 var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}];
 console.log(frutas);
 alert (frutas[1].nome);
+*/
 
 /*
 var fruta = {nome: "maça", cor: "vermelha"};
